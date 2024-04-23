77e460e66d4fede927e26f516c6b5414bb9c306d
chivicoko <victor.c.okoye@gmail.com>
2024-04-23 15:49:12 +0100

    add shortcuts for the dropdown menu items

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ class MainWindow(QMainWindow):
         self.actionOpen.triggered.connect(self.openFile)
         self.actionSave.triggered.connect(self.saveFile)
         self.actionSave_As.triggered.connect(self.saveFileAs)
+        self.actionUndo.triggered.connect(self.undo)
+        self.actionRedo.triggered.connect(self.redo)
+        self.actionCopy.triggered.connect(self.copy)
+        self.actionCUt.triggered.connect(self.cut)
+        self.actionPaste.triggered.connect(self.paste)
+        self.actionDark_Mode.triggered.connect(self.darkMode)
+        self.actionLight_Mode.triggered.connect(self.lightMode)
+        self.actionChange_FontSize.triggered.connect(self.changeFontSize)
 
     def newFile(self):
         print("New file")
@@ -23,6 +31,30 @@ class MainWindow(QMainWindow):
 
     def saveFileAs(self):
         print("Saved file as")
+
+    def undo(self):
+        print("Undo action on ...")
+
+    def redo(self):
+        print("Redo action on ...")
+
+    def copy(self):
+        print("Copied text")
+
+    def cut(self):
+        print("Cut text")
+
+    def paste(self):
+        print("Pasted text")
+
+    def darkMode(self):
+        print("Dark Mode activated")
+
+    def lightMode(self):
+        print("Light Mode activated")
+
+    def changeFontSize(self):
+        print("Changed text's font size")
 
 
 
